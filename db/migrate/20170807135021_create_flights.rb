9a15ecc7a4d1a6cbc8fe0df5fc7af513bb56a0fd
kayaneuh <n.kaliane@gmail.com>
2017-08-07 23:00:38 +0200

    Flight Airport models sont créés mais departure_aiport est une date au lieu de string

--- db/migrate/20170807135021_create_flights.rb
+++ db/migrate/20170807135021_create_flights.rb
@@ -1,12 +1,14 @@
 class CreateFlights < ActiveRecord::Migration[5.1]
   def change
-    create_table :flights do |t|
-      t.integer :duration
-      t.date :departure_date
-      t.date :departure_airport
-      t.string :arrival_aiport
 
+    create_table :flights do |t|
+
+      t.integer :duration
+      t.datetime :departure_date
+      t.belongs_to :departure_airport, index: true
+      t.belongs_to :arrival_airport, index: true
       t.timestamps
+
     end
   end
 end
